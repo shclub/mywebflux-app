b4cb6c24d17ad31e835f42aae6beacaf6af611f0
shclub <shclub@gmail.com>
2023-03-31 14:45:38 +0900

    springdoc swagger added

--- src/main/java/kt/reactive/mywebflux/router/CustomerRouterFunction.java
+++ src/main/java/kt/reactive/mywebflux/router/CustomerRouterFunction.java
@@ -1,16 +1,55 @@
 package kt.reactive.mywebflux.router;
 
+import kt.reactive.mywebflux.entity.Customer;
 import kt.reactive.mywebflux.handler.CustomerHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn; import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
+
 @Configuration
 public class CustomerRouterFunction {
+
+    @RouterOperations({
+            @RouterOperation(path = "/router/r2customers", method = RequestMethod.GET,
+                    beanClass = CustomerHandler.class, beanMethod = "getCustomers"),
+            @RouterOperation(path = "/router/r2customers/{id}", method = RequestMethod.GET,
+                    beanClass = CustomerHandler.class, beanMethod = "getCustomer",
+                    operation = @Operation(operationId = "getCustomer",
+                            parameters = {@Parameter(in = ParameterIn.PATH, name = "id")})
+            ),
+            @RouterOperation(path = "/router/r2customers", method = RequestMethod.POST,
+                    beanClass = CustomerHandler.class, beanMethod = "saveCustomer",
+                    operation = @Operation(operationId = "saveCustomer",
+                            requestBody = @RequestBody(content =
+                            @Content(schema = @Schema(implementation = Customer.class))))
+            ),
+            @RouterOperation(path = "/router/r2customers/{id}", method = RequestMethod.PUT,
+                    beanClass = CustomerHandler.class, beanMethod = "updateCustomer",
+                    operation = @Operation(operationId = "updateCustomer",
+                            parameters = {@Parameter(in = ParameterIn.PATH, name = "id")},
+                            requestBody = @RequestBody(content =
+                            @Content(schema = @Schema(implementation = Customer.class))))
+            ),
+            @RouterOperation(path = "/router/r2customers/{id}", method = RequestMethod.DELETE,
+                    beanClass = CustomerHandler.class, beanMethod = "deleteCustomer",
+                    operation = @Operation(operationId = "deleteCustomer",
+                            parameters = {@Parameter(in = ParameterIn.PATH, name = "id")})
+            )
+    })
+
     @Bean
     public RouterFunction<ServerResponse> routerFunction(CustomerHandler customerHandler) {
         //route() 메서드이 2번째 아규먼트 HandlerFunction
